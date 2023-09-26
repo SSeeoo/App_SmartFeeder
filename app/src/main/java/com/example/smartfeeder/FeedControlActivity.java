8267bfa03a3eebd938ba4765f86c60cdbaa7272f
SSeeoo <96329914+SSeeoo@users.noreply.github.com>
2023-09-26 12:05:04 +0900

    git commit

--- app/src/main/java/com/example/smartfeeder/FeedControlActivity.java
+++ app/src/main/java/com/example/smartfeeder/FeedControlActivity.java
@@ -1,5 +1,8 @@
 package com.example.smartfeeder;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -8,11 +11,18 @@ import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FeedControlActivity extends AppCompatActivity {
 
+    private NumberPicker hourPicker, minutePicker;
     private Button feedButton;
-    private NumberPicker numberPicker;
     private CountDownTimer countDownTimer;
 
     @Override
@@ -21,32 +31,98 @@ public class FeedControlActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed_control);
 
         feedButton = findViewById(R.id.feedButton);
-        numberPicker = findViewById(R.id.numberPicker);
+        hourPicker = findViewById(R.id.hourPicker);
+        minutePicker = findViewById(R.id.minutePicker);
 
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(60); // 예를 들어, 1분에서 60분 사이의 값
+        hourPicker.setMinValue(0);
+        hourPicker.setMaxValue(24);
+        hourPicker.setWrapSelectorWheel(true);
+
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        minutePicker.setWrapSelectorWheel(true);
 
         feedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 먹이 사출 기능 호출
                 dispenseFeed();
-
-                // 타이머 설정
-                int minutes = numberPicker.getValue();
-                startTimer(minutes * 60 * 1000); // 밀리초로 변환
+                int hours = hourPicker.getValue();
+                int minutes = minutePicker.getValue();
+                startTimer((hours * 60 + minutes) * 60 * 1000); // 밀리초로 변환
             }
         });
     }
 
     private void dispenseFeed() {
-        // Retrofit을 사용하여 서버에 feedDispense 요청을 보냅니다.
-        //
-        // 응답을 받으면, Toast 메시지를 표시합니다.
+        // 0시간 0분으로 설정된 경우, 요청을 보내지 않습니다.
+        // Retrofit 인스턴스를 생성하고, 요청을 보냅니다.
+        int hours = hourPicker.getValue();
+        int minutes = minutePicker.getValue();
 
+        if (hours == 0 && minutes == 0) {
+            Toast.makeText(this, "Cannot dispense feed for 0 hours and 0 minutes", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        Toast.makeText(this, "Feed dispensed", Toast.LENGTH_SHORT).show();
+        int totalMinutes = (hours * 60) + minutes;
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.5:5000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        SmartFeederApi api = retrofit.create(SmartFeederApi.class);
+        Call<Void> call = api.dispenseFeed(totalMinutes);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(FeedControlActivity.this, "Feed dispensed", Toast.LENGTH_SHORT).show();
+                    // Check SharedPreferences and Send Notification if needed
+                    sendNotificationIfNeeded(); // <- 이 부분이 알림을 보내는 코드를 호출하는 부분입니다.
+                } else {
+                    Toast.makeText(FeedControlActivity.this, "Failed to dispense feed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(FeedControlActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    // 이 함수를 추가하여 SharedPreferences를 확인하고 필요하다면 알림을 보냅니다.
+    private void sendNotificationIfNeeded() {
+        // TODO: Change "Settings" and "motor_notification" to the actual SharedPreferences and key you are using
+        boolean isNotificationOn = getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("motor_notification", false);
+
+        if (isNotificationOn) {
+            sendNotification();
+        }
+    }
+
+    // 이 함수를 추가하여 실제로 알림을 보냅니다.
+    private void sendNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        String channelId = "feed_notification_channel";
+        String channelName = "Feeder Notification";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher) // Set your notification icon here
+                .setContentTitle("Feed Dispensed")
+                .setContentText("The feeder has dispensed the feed.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(1, builder.build());
+    }
+
 
     private void startTimer(long millisInFuture) {
         if (countDownTimer != null) {
@@ -56,7 +132,7 @@ public class FeedControlActivity extends AppCompatActivity {
         countDownTimer = new CountDownTimer(millisInFuture, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                // 각 틱마다 UI를 업데이트할 수 있습니다.
+                // UI 업데이트를 여기에 추가할 수 있습니다.
             }
 
             @Override
--- app/src/main/java/com/example/smartfeeder/MainActivity.java
+++ app/src/main/java/com/example/smartfeeder/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.smartfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.view.View;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView settingsIcon = findViewById(R.id.settingsIcon); // 아이콘의 ID에 맞게 변경해주세요.
+        settingsIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
