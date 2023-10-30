c189c39e8bfc45974ec07959110c992e307773af
SSeeoo <96329914+SSeeoo@users.noreply.github.com>
2023-10-30 15:17:26 +0900

    git commit

--- app/src/main/java/com/example/smartfeeder/HistoryActivity.java
+++ app/src/main/java/com/example/smartfeeder/HistoryActivity.java
@@ -1,15 +1,11 @@
 package com.example.smartfeeder;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import androidx.annotation.NonNull;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -18,29 +14,16 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        ListView historyList = findViewById(R.id.historyList);
+        WebView historyWebView = findViewById(R.id.historyWebView);
 
-        fetchFeedHistory(historyList);
-    }
+        // WebView 설정
+        WebSettings webSettings = historyWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  // 웹 페이지의 JavaScript를 활성화합니다.
 
-    private void fetchFeedHistory(final ListView listView) {
-        ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
-        Call<List<FeedHistory>> call = apiInterface.getFeedHistory();
+        // WebViewClient 설정
+        historyWebView.setWebViewClient(new WebViewClient());
 
-        call.enqueue(new Callback<List<FeedHistory>>() {
-            @Override
-            public void onResponse(@NonNull Call<List<FeedHistory>> call, @NonNull Response<List<FeedHistory>> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    List<FeedHistory> feedHistories = response.body();
-                    ArrayAdapter<FeedHistory> adapter = new ArrayAdapter<>(HistoryActivity.this, android.R.layout.simple_list_item_1, feedHistories);
-                    listView.setAdapter(adapter);
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<List<FeedHistory>> call, @NonNull Throwable t) {
-                t.printStackTrace();
-            }
-        });
+        // 웹 페이지 로드
+        historyWebView.loadUrl("http://ec2-54-180-120-1.ap-northeast-2.compute.amazonaws.com:5000/detection_log");
     }
 }
