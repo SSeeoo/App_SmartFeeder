8f7cb279c165ad01855118c77a297fb6ce639644
SSeeoo <96329914+SSeeoo@users.noreply.github.com>
2023-10-30 15:29:52 +0900

    git commit

--- app/src/main/java/com/example/smartfeeder/HistoryActivity.java
+++ app/src/main/java/com/example/smartfeeder/HistoryActivity.java
@@ -20,6 +20,9 @@ public class HistoryActivity extends AppCompatActivity {
         WebSettings webSettings = historyWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);  // 웹 페이지의 JavaScript를 활성화합니다.
 
+        // 모바일 User Agent로 설정
+        webSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
+
         // WebViewClient 설정
         historyWebView.setWebViewClient(new WebViewClient());
 
